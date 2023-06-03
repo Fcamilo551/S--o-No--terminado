e6226f130b0e22e2c73d9a36115d6a73da2108f3
freydercl2004@gmail.com <freydercl2004@gmail.com>
2023-06-02 23:16:55 -0500

    camilo-com

---
+++ index.js
@@ -0,0 +1,18 @@
+const yesbtn = document.querySelector('#yesbtn');
+
+yesbtn.addEventListener('click',function () {
+  alert('sabia que ibas a decir que sí :3')
+});
+
+const nobtn = document.querySelector('#nobtn');
+
+nobtn.addEventListener('mouseover', function () {
+    const randomX = parseInt(Math.random()*100);
+    const randomY = parseInt(Math.random()*100);
+    nobtn.style.setProperty('top',randomY+'%');
+    nobtn.style.setProperty('left',randomX+'%');
+    nobtn.style.setProperty('transform',`translate(-${randomX}%,-${randomY}%)`);
+})
+nobtn.addEventListener('click',function() {
+  alert('Perdon esta nunca fue una opción ;)')
+}); 
